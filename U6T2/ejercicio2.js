0561edc241e5db6c45ef64c92e50e0dc0bc79ac3
alftacmir <alum.ataconm.2021@iesalixar.org>
2023-01-11 21:25:05 +0100

    ejercicio 2 para terminar

---
+++ U6T2/ejercicio2.js
@@ -0,0 +1,20 @@
+window.onload = iniciar();
+let boton = document.getElementById("button");
+boton.addEventListener("click",add,false);
+
+// Crea una página web que tenga un listado de tipo <ul> con un <li> de muestra.
+// Introduce un botón en la página que, cuando lo pulses, te muestre un prompt para que el usuario introduzca un texto.
+// Una vez cerrado el prompt el valor se añadirá como un nuevo<li> a la lista creada.
+// Añade dos botones más con texto “Borrar primer li” y “Borrar último li” de modo que cuando pulses el primer botón borre el primer elemento de la lista y cuando pulses el último borre el último elemento de la lista.
+
+function iniciar(){
+
+}
+
+function add(){
+    let contenido = prompt("Introduce el nuevo elemento de la lista")
+    let li_Creado = document.createElement("li");
+    li_Creado.
+
+
+}
